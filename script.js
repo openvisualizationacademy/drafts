e197dbdc494288862301b39037f96f94a5138495
Vinicius Sueiro <hi@vsueiro.com>
2024-09-28 10:21:21 -0400

    Interpolate colors and add random coordinates (looks terrible most of the time)

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 const container = document.querySelector("#container");
 
 // Options
-const side = 144;
+const side = 168;
+const margin = 24;
 const steps = 255;
 const segments = 6;
+const thickness = 1;
 const cell = side / segments;
 
+// Default logo
 const from = {
   coords: [cell * 1, cell * 5, cell * 6, cell * 1],
+  color: "#f0305d",
 };
 
 const to = {
   coords: [cell * 0, cell * 3, cell * 5, cell * 6],
+  color: "#ffc20a",
 };
+
+// Randomize Coordinates
+const generator = d3.randomInt(segments + 1);
+const randomCell = () => generator() * cell;
+
+from.coords = [...Array(4)].map(() => randomCell());
+to.coords = [...Array(4)].map(() => randomCell());
 
 // Namespace
 const ns = "http://www.w3.org/2000/svg";
@@ -27,7 +39,11 @@ container.replaceChildren();
 
 // Create SVG
 const svg = document.createElementNS(ns, "svg");
-svg.setAttribute("viewBox", `0 0 ${side} ${side}`);
+
+const totalSize = side + margin * 2;
+
+svg.setAttribute("viewBox", `${-margin} ${-margin} ${totalSize} ${totalSize}`);
+svg.style.width = `${totalSize}px`;
 container.append(svg);
 
 // Create interpolator
@@ -46,8 +62,8 @@ for (let i = 0; i <= steps; i++) {
   const path = document.createElementNS(ns, "path");
   path.setAttribute("d", `M${current.coords}`);
 
-  path.setAttribute("stroke", "black");
-  path.setAttribute("stroke-width", ".5");
+  path.setAttribute("stroke", current.color);
+  path.setAttribute("stroke-width", thickness);
 
   svg.append(path);
 }
